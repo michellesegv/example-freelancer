f8cc9857b2b315ba9e1e6291143eae8f8597a650
michellesegv <michelle@laboratoria.la>
2018-03-12 02:52:36 -0500

    add comments

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,7 +2,7 @@ const { resolve } = require('path')
 
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+// 
 module.exports = {
   context:  resolve(__dirname, 'src'),
   entry: './index.js',
